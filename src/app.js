a138ed79f5ae9c49ec41c9c30dac15bb237cf28f
Harry37x2 <bartek3737@o2.pl>
2023-01-19 19:39:28 +0100

    feat: filtering by query and region

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import { renderCountriesList } from "./dom-utils.js";
 const API_URL_ALL='https://restcountries.com/v3.1/all'
+let countries;
+let query = '';
+let region = '';
 
 fetch(API_URL_ALL)
     .then((res)=> res.json())
     .then((countriesRawData) => {
-        const countries = countriesRawData.map((country) => {
+        countries = countriesRawData.map((country) => {
             return {
                 capital: country.capital && country.capital[0],
                 population: country.population,
@@ -15,3 +18,23 @@ fetch(API_URL_ALL)
         });
         renderCountriesList(countries)
     });
+
+    const filterAndRenderCountriesList = () => {
+        const filteredCountries = countries.filter((country) => {
+            return (
+                country.name.toLowerCase().includes(query) &&
+                (!region || country.region === region)
+            );
+        });
+        renderCountriesList(filteredCountries);
+    };
+
+    document.querySelector('#query').addEventListener('input',(e)=>{
+        query = e.target.value.toLowerCase().trim();
+        filterAndRenderCountriesList();
+    });
+
+    document.querySelector('#region').addEventListener('change',(e)=>{
+        region = e.target.value;
+        filterAndRenderCountriesList();
+    })
